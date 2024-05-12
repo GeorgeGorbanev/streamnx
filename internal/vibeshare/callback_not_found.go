f89c7beb2e3dbc56ea6264370929049c323fd3a5
GeorgeGorbanev <georgegorbanev@gmail.com>
2024-05-12 16:35:39 +0700

    feat: add button not available message

--- internal/vibeshare/callback_not_found.go
+++ internal/vibeshare/callback_not_found.go
@@ -8,4 +8,8 @@ import (
 
 func (vs *Vibeshare) callbackNotFoundHandler(callback *telegram.Callback) {
 	slog.Warn("callback not found", slog.String("data", callback.Data.Route))
+	vs.send(&telegram.Message{
+		To:   callback.Sender,
+		Text: "This button not available anymore. Try to request a new button 🙏",
+	})
 }
